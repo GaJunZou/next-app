821bb7760f94152b66e70cf96b80d18c9ab47d55
zoujiajin <zoujiajin@skieer.com>
2025-05-15 23:49:25 +0800

    initial gh-pages

--- guide.js
+++
@@ -1,4 +0,0 @@
-/*
-    以前，把 src/pages/api 作为api 文件，这种方法依赖于 Nodejs 请求/响应对象 和 和类似express的API。
-    在 v13 之后，App Router 完全支持web标准的请求/响应api（默认）。可以使用 route.js 代替 src/pages/api。
-*/
--- src/pages/start/index.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-    return <p>welcome to Start</p>
-}
